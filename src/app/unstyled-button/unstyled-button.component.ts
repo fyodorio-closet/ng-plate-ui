cdd67e48c0dde5bc557133b5ca8224b752b79174
Fyodor Loenko <fedor.loenko@scorpion.co>
2020-02-20 20:49:43 +0500

    add reset and bindings for unstyled button component

--- src/app/unstyled-button/unstyled-button.component.ts
+++ src/app/unstyled-button/unstyled-button.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'plate-unstyled-button',
   templateUrl: './unstyled-button.component.html',
   styleUrls: ['./unstyled-button.component.scss']
 })
-export class UnstyledButtonComponent {}
+export class UnstyledButtonComponent {
+  @Input() textLabel: string;
+  @Input() ariaLabel: string;
+  @Output() clickButton: EventEmitter<Event> = new EventEmitter();
+
+  onCLick($event: Event) {
+    this.clickButton.emit($event);
+  }
+}
